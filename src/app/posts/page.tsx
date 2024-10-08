bf457f1d5884743a17024955abdf3c41e7570136
BenBktech <bbrucher@gmail.com>
2024-10-08 11:22:10 +0200

    Displaying first post

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,7 +1,20 @@
+import Link from "next/link"
+import prisma from "@/lib/db"
+
 export default async function PostsPage() {
+
+    const posts = await prisma.post.findMany()
+
     return (
         <div>
             <h1>Posts</h1>
+            <ul>
+                {posts.map((post) => (
+                    <li key={post.id}>
+                        <Link href={`/posts/${post.id}`}>{post.title}</Link>
+                    </li>
+                ))}
+            </ul>
         </div>
     )
 }
